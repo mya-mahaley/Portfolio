99fba5aaeaa6963dc6f213f052431a9bc1ca49e1
mya-mahaley <mya.mahaley@yahoo.com>
2023-05-19 21:39:45 -0500

    Home page almost done. TODO = Linking

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import Home from "./Components/Home";
+import Projects from "./Components/Projects";
+import Experience from "./Components/Experience";
+import { Suspense } from "react";
+import { Loader } from "@react-three/drei";
+import logo from "./Assets/Icons/logo.png"
 
+
+function Navigation() {
+  return (
+    <Navbar variant="dark">
+      <Container>
+      
+        <Nav className="ms-auto">
+          <Nav.Link className='NavBar' as={Link} to="/">HOME</Nav.Link>
+          <Nav.Link className='NavBar' as={Link} to="/Projects">PROJECTS</Nav.Link>
+          <Nav.Link className='NavBar' as={Link} to="/Projects">EXPERIENCE</Nav.Link>
+       </Nav>
+       <Navbar.Brand className="Brand"><img className='BrandImg' src={logo} alt="MM"/></Navbar.Brand>
+      </Container>
+    </Navbar>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Suspense fallback={null}>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/Experience" element={<Experience />} />
+          </Routes>
+         </Suspense>
+      </div>
   );
 }
 
---
+++ my-app/src/Components/Projects.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css"
+import { Row, Col, Container} from "react-bootstrap";
+export default function Projects() {
+    return (       
+        <div>
+            <body>
+                
+                <h2>EXPLORE MY RECENT</h2>
+                <h1 className="title">PROJECTS</h1>
+            </body>  
+        </div>
+    );
+}
